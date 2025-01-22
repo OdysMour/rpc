7c6c178dcc8d730ec1743690545f5d4fe4764de6
OdysMour <it21566@hua.gr>
2025-01-22 18:35:41 +0200

    small parent fix

--- rpc.c
+++ rpc.c
@@ -6,7 +6,10 @@
 #include <string.h>
 #include <sys/select.h>
 #include <sys/sem.h>
+#include <sys/ipc.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 void write_message(const char *filename, const char *process_name, pid_t pid)
 {
@@ -37,7 +40,7 @@ void write_message(const char *filename, const char *process_name, pid_t pid)
 
 int main(int argc, char *argv[])
 {
-    int desc_ready,rc = 0;
+    int desc_ready, rc = 0;
     char response[5];
     if (argc != 3)
     {
@@ -45,13 +48,17 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-/* Simple semaphore used here is actually a set of 1                  */
-int                     semaphoreId=-1;
+    /* Simple semaphore used here is actually a set of 1                  */
+    int semaphoreId = -1;
 
-  semaphoreId = semget(IPC_PRIVATE, 1, S_IRUSR|S_IWUSR);
-  if (semaphoreId < 0) { printf("semget failed, err=%d\n",errno); exit(1); }
-  /* Set the semaphore (#0 in the set) count to 1. Simulate a mutex */
-  rc = semctl(semaphoreId, 0, SETVAL, (int)1);
+    semaphoreId = semget(IPC_PRIVATE, 1, S_IRUSR | S_IWUSR);
+    if (semaphoreId < 0)
+    {
+        printf("semget failed, err=%d\n", errno);
+        exit(1);
+    }
+    /* Set the semaphore (#0 in the set) count to 1. Simulate a mutex */
+    rc = semctl(semaphoreId, 0, SETVAL, (int)1);
 
     const char *filename = argv[1];
     int N = atoi(argv[2]);
@@ -106,29 +113,41 @@ int                     semaphoreId=-1;
             close(pipes1[i][1]); // Close write end
             close(pipes2[i][0]); // Close read end
             // Read message from parent
-            char message[100];
-            read(pipes1[i][0], message, sizeof(message));
+            do
+            {
+                char message[100];
+                // Sem
+                int rc;
+                struct sembuf lockOperation = {0, -i - 1, 0};
+                struct sembuf unlockOperation = {0, ((i + 2) % (N + 1)) == 0 ? 1 : (i + 2) % (N + 1), 0};
+                // fprintf(stderr, "C%d --> %d\n", i + 1, semctl(semaphoreId, 0, GETVAL));
 
-            // Sem
-               int   rc;
-               struct sembuf lockOperation = { 0, -i-1, 0};
-               struct sembuf unlockOperation = { 0, (i+2)%(N+1), 0};
-               rc = semop(semaphoreId, &lockOperation, 1);
-            
+                read(pipes1[i][0], message, sizeof(message));
+                fprintf(stderr, "C%d: %s, semval=%d\n", i + 1, message, semctl(semaphoreId, 0, GETVAL));
 
+                rc = semop(semaphoreId, &lockOperation, 1);
+                if (rc < 0)
+                {
+                    perror("  semop failed");
+                    break;
+                }
+                // fprintf(stderr, "C%d --> %d\n", i + 1, semctl(semaphoreId, 0, GETVAL));
 
-            // Write to file
-            char process_name[10];
-            snprintf(process_name, sizeof(process_name), "C%d", i + 1);
-            write_message(filename, process_name, getpid());
-sleep(2);
-               rc = semop(semaphoreId, &unlockOperation, 1);
+                // Write to file
+                char process_name[12];
+                snprintf(process_name, sizeof(process_name), "C%d", i + 1);
+                write_message(filename, process_name, getpid());
+                char process_num[12];
+                snprintf(process_num, sizeof(process_num), "%d", ((i + 2) % (N + 1)) == 0 ? 1 : (i + 2) % (N + 1));
+                write_message(filename, process_num, getpid());
+                //sleep(0.1);
+                rc = semop(semaphoreId, &unlockOperation, 1);
+                // fprintf(stderr, "C%d --> %d\n", i + 1, semctl(semaphoreId, 0, GETVAL));
 
-            
-            // Send response to parent
-            char response[] = "done";
-            write(pipes2[i][1], response, sizeof(response));
-
+                // Send response to parent
+                char response[] = "done";
+                write(pipes2[i][1], response, sizeof(response));
+            } while (1);
             // close(pipes1[i][0]);
             // close(pipes2[i][1]);
             // return 0;
@@ -139,21 +158,20 @@ sleep(2);
             child_pids[i] = pid;
         }
     }
-/* Simple lock operation. 0=which-semaphore, -1=decrement, 0=noflags  */
-//struct sembuf lockOperation = { 0, -1, 0};
-/* Simple unlock operation. 0=which-semaphore, 1=increment, 0=noflags */
-//struct sembuf unlockOperation = { 0, 1, 0};
+    /* Simple lock operation. 0=which-semaphore, -1=decrement, 0=noflags  */
+    // struct sembuf lockOperation = { 0, -1, 0};
+    /* Simple unlock operation. 0=which-semaphore, 1=increment, 0=noflags */
+    // struct sembuf unlockOperation = { 0, 1, 0};
 
     // Add select() to see if children are done
     // Create a set of file descriptors to watch
-
     // Send messages to children
     char message[100];
     for (int i = 0; i < N; i++)
     {
         snprintf(message, sizeof(message),
                  "Hello child, I am your father and I call you: C%d", i + 1);
-        write(pipes1[i][1], message, strlen(message) + 1);
+        write(pipes1[i][1], message, strlen(message));
         close(pipes1[i][0]);
         // char response[5];
         // read(pipes2[i][0], response, sizeof(response));
@@ -163,10 +181,11 @@ sleep(2);
 
     fd_set read_set, working_set;
     struct timeval timeout;
-    int max_fd = 0;
+    int max_fd = sizeof(pipes2);
 
     // Clear the set
     FD_ZERO(&read_set);
+    FD_ZERO(&working_set);
 
     // Add all pipe read ends to the set
     for (int i = 0; i < N; i++)
@@ -178,55 +197,66 @@ sleep(2);
         }
     }
 
-    // Set timeout to 5 seconds
-    timeout.tv_sec = 5;
+    // Set timeout
+    timeout.tv_sec = 60;
     timeout.tv_usec = 0;
+
     do
     {
         memcpy(&working_set, &read_set, sizeof(read_set));
-        printf("Waiting on select()...\n");
 
+        printf("Waiting on select()...\n");
+        /* https://www.ibm.com/docs/en/ztpf/2024?topic=apis-select-monitor-read-write-exception-status */
         // Wait for any pipe to be ready
         int ret = select(max_fd + 1, &working_set, NULL, NULL, &timeout);
-        if (ret == -1)
+        /**********************************************************/
+        /* Check to see if the select call failed.                */
+        /**********************************************************/
+        if (ret < 0)
         {
-            perror("select");
-            return 1;
+            perror("  select() failed");
+            break;
         }
-        else if (ret == 0)
+
+        /**********************************************************/
+        /* Check to see if the 3 minute time out expired.         */
+        /**********************************************************/
+        if (ret == 0)
         {
-            printf("Timeout occurred! No data after 5 seconds.\n");
-            return 1;
+            printf("  select() timed out.  End program.\n");
+            break;
         }
-        else
+
+        desc_ready = ret;
+        fprintf(stderr, "%d\n", desc_ready);
+
+        for (int i = 0; i <= max_fd && desc_ready > 0; i++)
         {
-            desc_ready = ret;
-            for (int i = 0; i <= max_fd && desc_ready > 0; ++i)
+            if (FD_ISSET(pipes2[i][0], &working_set))
             {
-                if (FD_ISSET(i, &working_set))
+                desc_ready--;
+                do
                 {
-                    desc_ready--;
-                    do
+                    ssize_t rc = read(pipes2[i][0], response, sizeof(response));
+                    if (rc < 0)
                     {
-                        ssize_t rc = read(i, response, sizeof(response));
-                        if (!rc > 0)
-                        {
-                            perror("read");
-                            break;
-                        }
-                        ssize_t len = rc;
-                        snprintf(message, sizeof(message),
-                                 "Hello child, I am your father and I call you: C%d", i + 1);
-                        rc = write(pipes1[i][1], message, strlen(message) + 1);
-                        if (!rc > 0)
-                        {
-                            perror("write");
-                            break;
-                        }
-                    } while (1);
-                }
+                        fprintf(stderr, "read error on pipe %d: %s\n", i, strerror(errno));
+                        break;
+                    }
+                    ssize_t len = rc;
+                    snprintf(message, sizeof(message),
+                             "Hello child, I am your father and I call you: C%d", i + 1);
+                    rc = write(pipes1[i][1], message, strlen(message));
+                    if (rc <= 0)
+                    {
+                        perror("write");
+                        break;
+                    }
+                    break;
+                } while (1);
             }
         }
+
     } while (1);
     // Wait for all children
     for (int i = 0; i < N; i++)
